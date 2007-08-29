9ebcb9f24dc448e54c8efdc18be480d006dabd3c
peacebird <peacebird@00473811-6f2b-0410-a651-d1448cebbc4e>
2007-08-29 19:36:00 +0000

    Added items remotely
    F:\project\RssReader\active_object\ActiveObject.cpp
    F:\project\RssReader\active_object\ConnectRequest.cpp
    F:\project\RssReader\active_object\IMethodRequest.cpp
    F:\project\RssReader\active_object\main.cpp
    F:\project\RssReader\active_object\Proxy.cpp
    F:\project\RssReader\active_object\Scheduler.cpp
    F:\project\RssReader\active_object\Servent.cpp
    F:\project\RssReader\active_object\libcurl.dll
    F:\project\RssReader\active_object\zlib1.dll
    F:\project\RssReader\active_object\active_object.DPbcd
    F:\project\RssReader\active_object\ActiveObject.h
    F:\project\RssReader\active_object\ConnectRequest.h
    F:\project\RssReader\active_object\def.h
    F:\project\RssReader\active_object\event.h
    F:\project\RssReader\active_object\IMethodRequest.h
    F:\project\RssReader\active_object\Proxy.h
    F:\project\RssReader\active_object\rssreader.h
    F:\project\RssReader\active_object\Scheduler.h
    F:\project\RssReader\active_object\Servent.h
    F:\project\RssReader\active_object\libboost_thread-vc80-mt-1_34.lib
    F:\project\RssReader\active_object\libboost_thread-vc80-mt-gd-1_34.lib
    F:\project\RssReader\active_object\libcurl.lib
    F:\project\RssReader\active_object\libcurl_imp.lib
    F:\project\RssReader\active_object\rssreader.lib
    F:\project\RssReader\active_object\rssreaderd.lib
    F:\project\RssReader\active_object\active_object.sln
    F:\project\RssReader\active_object\active_object.vcproj

---
+++ active_object/rssreader.h
@@ -0,0 +1,21 @@
+#include "def.h"
+#include <boost/shared_ptr.hpp>
+
+class CRssReaderImpl;
+
+//#pragma comment( lib , "libcurl_imp.lib" )
+
+//#ifdef _DEBUG
+//	#pragma comment( lib , "rssreaderd.lib")
+//#else
+//	#pragma comment( lib , "rssreader.lib")
+//#endif
+
+class CRssReader
+{
+public:
+	void GetRss( const tstring& url, tstring& xml );
+	CRssReader();
+private:
+	boost::shared_ptr<CRssReaderImpl> m_RssImpl;
+};
